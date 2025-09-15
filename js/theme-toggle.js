7e15ec0bd33ee0792dbea40bdfac8135683e2156
Seif Eldien Ahmad Mohammad <seifahmed7293@gmail.com>
2025-09-15 13:37:46 -0400

    Update theme-toggle.js

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -46,3 +46,12 @@ window.onload = () => {
     reloadDisqus(); // لو موجودة
   }
 };
+// mobile menu
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerButton = document.querySelector('.hamburger-button');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  hamburgerButton.addEventListener('click', () =>
+    mobileMenu.classList.toggle('active')
+  );
+});
